89ec7c4431a10fe2fd0dd46e284ae9408c29a18a
Ali Yasin Emir <aliyasin.emir16@gmail.com>
2023-06-06 13:10:57 +0300

    I've implemented the hamburger menu function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,37 @@
 /*=============== SHOW MENU ===============*/
 
+const navClose = document.getElementById('nav__close');
+const navToggle = document.getElementById('nav__toggle');
+const navMenu = document.getElementById('nav__menu');
+
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
+    navClose.style.display = 'block';
+    navMenu.classList.add('show-menu');
+  });
+}
+
+if (navClose) {
+  navClose.addEventListener('click', () => {
+    navClose.style.display = 'none';
+    navMenu.classList.remove('show-menu');
+  });
+}
+
+// const showMenu = () => {
+//   navMenu.classList.add('show-menu');
+// };
+// toggle.addEventListener('click', showMenu);
+
+const close2 = document.getElementById('');
+const close3 = document.getElementById('');
 
 /*=============== REMOVE MENU MOBILE ===============*/
 
-
 /*=============== ADD BLUR TO HEADER ===============*/
 
-
-/*=============== SHOW SCROLL UP ===============*/ 
-
+/*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
